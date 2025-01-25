d87ee505784e04013a8b15a11b7a7be37acdcc9c
agilimam <agilimamalhafizi04@gmail.com>
2025-01-26 03:27:07 +0700

    inisialisai homeviemodel repository pasienhewan dan jenishewanrepository

---
+++ app/src/main/java/com/example/uaspam/ui/ViewModel/PenyediaViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.uaspam.ui.ViewModel
+
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.uaspam.PetShopApplication
+import com.example.uaspam.ui.ViewModel.Dokter.HomeDokterViewModel
+import com.example.uaspam.ui.ViewModel.Dokter.InsertDokterViewModel
+import com.example.uaspam.ui.ViewModel.Dokter.UpdateDokterViewModel
+import com.example.uaspam.ui.ViewModel.JenisHewan.HomeJenisViewModel
+import com.example.uaspam.ui.ViewModel.JenisHewan.InsertJenisViewModel
+import com.example.uaspam.ui.ViewModel.JenisHewan.UpdateJenisViewModel
+import com.example.uaspam.ui.ViewModel.PasienHewan.DetailViewModel
+import com.example.uaspam.ui.ViewModel.PasienHewan.HomeViewModel
+import com.example.uaspam.ui.ViewModel.PasienHewan.InsertViewModel
+import com.example.uaspam.ui.ViewModel.PasienHewan.UpdateViewModel
+import com.example.uaspam.ui.ViewModel.Perawatan.DetailPrwViewModel
+import com.example.uaspam.ui.ViewModel.Perawatan.HomePerawatanViewModel
+import com.example.uaspam.ui.ViewModel.Perawatan.InsertPrwViewModel
+import com.example.uaspam.ui.ViewModel.Perawatan.UpdatePrwViewModel
+
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiHewan().container.pasienHewanRepository,aplikasiHewan().container.jenisHewanRepository)}
+    }
+}
+
+fun CreationExtras.aplikasiHewan(): PetShopApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PetShopApplication)
