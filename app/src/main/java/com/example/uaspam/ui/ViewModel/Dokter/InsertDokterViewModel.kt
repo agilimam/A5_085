61512f26e8e19a9368283c0b1220c17fffdce71d
agilimam <agilimamalhafizi04@gmail.com>
2025-01-27 13:34:16 +0700

    menambahkan ViewModel HomeDokter untuk memuat dan menghapus data dokter

---
+++ app/src/main/java/com/example/uaspam/ui/ViewModel/Dokter/InsertDokterViewModel.kt
@@ -0,0 +1,63 @@
+package com.example.uaspam.ui.ViewModel.Dokter
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uaspam.model.Dokter
+import com.example.uaspam.model.Jenishewan
+import com.example.uaspam.repository.DokterRepository
+import com.example.uaspam.repository.JenisHewanRepository
+import kotlinx.coroutines.launch
+
+
+class InsertDokterViewModel (private val Dk : DokterRepository) : ViewModel() {
+
+    var uiState by mutableStateOf(InsertDkUiState())
+        private set
+
+    fun updateInsertDkState(insertDkUiEvent: InsertDkUiEvent) {
+        uiState = InsertDkUiState(insertDkUiEvent = insertDkUiEvent)
+    }
+
+    suspend fun insertDk(){
+        viewModelScope.launch {
+            try {
+                Dk.insertDokter(uiState.insertDkUiEvent.toDk())
+            } catch (e:Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertDkUiState(
+    val insertDkUiEvent: InsertDkUiEvent = InsertDkUiEvent()
+)
+
+data class InsertDkUiEvent(
+    val id_dokter: Int? = null,
+    val nama_dokter : String = "",
+    val spesialisasi : String = "",
+    val kontak : String = "",
+
+    )
+
+fun InsertDkUiEvent.toDk(): Dokter = Dokter (
+    id_dokter = id_dokter ?: 0,
+    nama_dokter = nama_dokter,
+    spesialisasi = spesialisasi,
+    kontak = kontak,
+
+    )
+
+fun Dokter.toUiStateDk(): InsertDkUiState = InsertDkUiState(
+    insertDkUiEvent = toInsertDkUiEvent()
+)
+
+fun Dokter.toInsertDkUiEvent(): InsertDkUiEvent = InsertDkUiEvent (
+    nama_dokter = nama_dokter,
+    spesialisasi = spesialisasi,
+    kontak = kontak,
+)
